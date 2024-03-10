384830afd4c4a3f0ad2af3e100016d62c532123b
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-03-10 12:17:59 +0700

    add db

--- xmltojson.py
+++ xmltojson.py
@@ -1,8 +1,8 @@
 import xmltodict
 import json
 
-with open("dictionary.xml", "r", encoding="utf-8") as fr:
+with open("result_a.xml", "r", encoding="utf-8") as fr:
   xml_string = fr.read()
 python_dict = xmltodict.parse(xml_string)
-with open('dictionary.json', 'w', encoding='utf-8') as fp:
+with open('dictionary_a.json', 'w', encoding='utf-8') as fp:
   json.dump(python_dict, fp, ensure_ascii=False, indent=2)
